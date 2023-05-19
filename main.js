c5c3cc461f699306a70b1fee9c7a7d171c0960dc
Ange1D <47231898+Ange1D@users.noreply.github.com>
2023-05-19 16:36:41 -0600

    add function descifrar

--- main.js
+++ main.js
@@ -41,3 +41,24 @@ function cifrar(mensaje) {
     document.getElementById('message-output').value = mensajeCifrado;
 
   }
+
+function btnDescifrar(){
+    var mensaje = document.getElementById('message-input').value;
+    if(mensaje == "" || mensaje.length == 0){
+        alert("Ingrese un mensaje");
+    }else{
+        descifrar(mensaje); 
+    }
+    
+}
+
+function descifrar(mensaje) {
+    let mensajeDescifrado = mensaje.toLowerCase();
+    mensajeDescifrado = mensajeDescifrado.replace(/enter/g, "e");
+    mensajeDescifrado = mensajeDescifrado.replace(/imes/g, "i");
+    mensajeDescifrado = mensajeDescifrado.replace(/ai/g, "a");
+    mensajeDescifrado = mensajeDescifrado.replace(/ober/g, "o");
+    mensajeDescifrado = mensajeDescifrado.replace(/ufat/g, "u");
+
+    document.getElementById('message-output').value = mensajeDescifrado;
+  }
